092199fe51b3d644ff3b59e2c898a261872fcbd9
DefinitelyNotWence <wencepineda1@gmail.com>
2025-08-15 22:03:44 +1000

    saving blocked sites

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,27 @@
 const userInput = document.getElementById("userInput");
 const addWebsite = document.getElementById("addWebsite");
 const myList = document.getElementById("myList");
+const blockedWebsites = [];
 
-addWebsite.addEventListener('click', function() {
+// Save list
+chrome.storage.sync.set({ blockedSites: ["facebook.com", "twitter.com"] });
+
+// Get list
+chrome.storage.sync.get(["blockedSites"], (result) => {
+    console.log(result.blockedSites || []);
+});
+
+
+addWebsite.addEventListener("click", function() {
     const textValue = userInput.value;
 
-    if (textValue.trim() !== '') { // Prevent adding empty items
-        const newListItem = document.createElement('li');
+    if (textValue.trim() !== "") { // Prevent adding empty items
+        const newListItem = document.createElement("li");
         const textNode = document.createTextNode(textValue);
         newListItem.appendChild(textNode);
         myList.appendChild(newListItem);
+        blockedWebsites.push(newListItem);
 
-        userInput.value = ''; // Clear the input field after adding
+        userInput.value = ""; // Clear the input field after adding
     }
 });
