c03bb04205d8b57aeecc2be5840651c23ac2ff86
TheRealKevinK <kyoga6820@gmail.com>
2025-08-16 09:58:41 +1000

    Created flask app with nothing but a health status checker.

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
-from flask import Flask, request
-import psycopg2
+from flask import Flask
 
-DATA_HOST = '10.89.76.206'
-DATABASE = 'postgres'
-USER = 'postgres'
-PASSWORD = '1234'
-PORT = '5432'
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    host_ip = request.host
-    return "Amogus irl" + host_ip
 
-"""
-Usage: call access database when needing to change anything.
-
-"""
-def access_database():
-    conn = psycopg2.connect(
-        host=DATA_HOST,
-        database=DATABASE,
-        user=USER,
-        password=PASSWORD,
-        port=PORT
-    )
-    
-    cur = conn.cursor()
-    # Insert SQL between the """
-    cur.execute("""
-    
-    """)
-    results = cur.fetchall() # Fetches all output from above query
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 
-"don't tell me what to do ai"
-
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
+    # This will run the Flask app on port 5001
