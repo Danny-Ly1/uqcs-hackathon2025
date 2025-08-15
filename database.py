e10f95e0dcf67d55b48f881258fa4f9e4ed3e854
Kevin Pan <kpan5121@gmail.com>
2025-08-16 03:22:27 +1000

    Update database.py

--- database.py
+++ database.py
@@ -7,9 +7,47 @@ PASSWORD = '1234'
 PORT = '5432'
 
 """
-Usage: call access database when needing to change anything.
+Creates connection to database
+"""
+def connect_database():
+    conn = psycopg2.connect(
+        host=DATA_HOST,
+        database=DATABASE,
+        user=USER,
+        password=PASSWORD,
+        port=PORT
+    )
+    return conn
 
 """
+Adds a new url into the blocklist
+"""
+def add_blocked_url(user_id: Optional[int], link: Optional[str]): # Make not optional later
+    with connect_database() as conn:
+
+        cur = conn.cursor()
+        cur.execute("""
+        SELECT * FROM groups
+        """)
+        groups = cur.fetchall() # Records result
+
+        cur.execute("""
+        SELECT * FROM users
+        """)
+        user_data = cur.fetchall()
+
+        group_id = -1
+        for i in user_data:
+            if i[0] == user_id:
+                group_id = i[GROUP_ID_INDEX]
+                break       
+        
+        cur.execute("""UPDATE groups SET links = ARRAY_APPEND(links, %s) 
+        WHERE groupid = %s""", (link, group_id)) # Append url to record
+        conn.commit()
+
+    conn.close()
+
 
 def access_database():
     conn = psycopg2.connect(
