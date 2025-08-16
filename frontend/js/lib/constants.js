cd77b916273d682cb345cddf481fc294ce18e7e0
David Wu <daichuan@dwu21.net>
2025-08-16 17:30:37 +1000

    frontend: migrate to service worker alarms for handling unlock time
    I am clinging onto my life by a fucking thread

---
+++ frontend/js/lib/constants.js
@@ -0,0 +1,11 @@
+// number enum
+export const SW_MESSAGE_TYPES = Object.freeze({
+    // SW_* implies msg intended for service worker
+    // CS_* implies msg intended for content script
+
+    // Get the SW to set a Chrome alarm and unlock the lockInState in the future
+    SW_SET_UNLOCK_ALARM: 0,
+
+    // Notify the content script that the store/cache has changed
+    CS_STORE_CHANGED: 1,
+});
--- frontend/js/lib/store.js
+++ frontend/js/lib/store.js
@@ -1,24 +1,23 @@
 'use strict'
-let browserStoreInit = false;
+
+import { SW_MESSAGE_TYPES } from "./constants.js";
+
 const cache = {};
 
-// This should be called in every imported function first
-export const waitForBrowserStoreInit = async () => {
-    if (browserStoreInit) return;
-    browserStoreInit = true;
-
+// This should be called before any storage operation, as we have both service worker and content scripts potentially modifying the store
+export const refreshCache = async () => {
     const browserStore = await chrome.storage.local.get();
 
     // Initialise the storageCache if the required key doesn't exist
     if (!browserStore.filterList || !browserStore.lockInState) {
         console.log('Extension local storage uninitialised, initialising...');
 
-        chrome.storage.local.clear();
+        await chrome.storage.local.clear();
 
         cache.filterList = [];
         cache.lockInState = {lockedIn: false, unlockTimeEpoch: 0};
 
-        chrome.storage.local.set(cache);
+        await chrome.storage.local.set(cache);
 
         return;
     }
@@ -33,6 +32,8 @@ export const waitForBrowserStoreInit = async () => {
 
 // id of filterList object shall be provided
 export const removeFilterById = async (filterId) => {
+    await refreshCache();
+
     const newFilterList = cache.filterList.filter(f => f.id !== filterId);
     if (newFilterList.length === cache.filterList.length) {
         throw new Error('filterId provided does not exist');
@@ -45,7 +46,8 @@ export const removeFilterById = async (filterId) => {
 
 // returns boolean based on if url is in filter list
 export const isURLInFilterList = async (url) => {
-    console.log(cache);
+    await refreshCache();
+
     const filtered = cache.filterList.filter(f => f.url == url);
     return filtered.length > 0;
 }
@@ -62,6 +64,8 @@ export const addFilter = async (url) => {
 
     // TODO: add filter into server so we get ID
     const newFilter = {id: Math.floor(Math.random() * (42000 - 69) + 69), url};
+
+    await refreshCache();
     cache.filterList.push(newFilter);
     await chrome.storage.local.set(cache);
 
@@ -69,7 +73,10 @@ export const addFilter = async (url) => {
 }
 
 // Return lockedInState object
-export const getLockedInState = () => cache.lockInState;
+export const getLockedInState = async () => {
+    await refreshCache();
+    return JSON.parse(JSON.stringify(cache.lockInState));
+}
 
 // Set lock in state to true with duration
 export const lockIn = async (lockInDurationSec) => {
@@ -80,16 +87,26 @@ export const lockIn = async (lockInDurationSec) => {
     // TODO: set lock-in on server side, and get unlock epoch from server side
     // TODO: sync with web worker to unlock even when extension popup is not on
 
+    await refreshCache();
+    console.log(cache);
+    if (cache.lockInState.lockedIn) {
+        throw new Error('Cannot lock-in when already locked in!');
+    }
+
     cache.lockInState.lockedIn = true;
-    cache.lockInState.unlockTimeEpoch = Math.floor((Date.now() / 1000) + (lockInDurationSec));
+    const unlockTimeEpoch = Math.floor((Date.now() / 1000) + (lockInDurationSec));
+    cache.lockInState.unlockTimeEpoch = unlockTimeEpoch;
 
     await chrome.storage.local.set(cache);
+    await chrome.runtime.sendMessage({type: SW_MESSAGE_TYPES.SW_SET_UNLOCK_ALARM, unlockTimeEpoch});
 }
 
 // Checks the current time, and compares it with the lock-in time
 // Returns true, when current time is beyond unlock time
 // TODO: come up with better method to do unlock when time is up
 export const updateLockInState = async () => {
+    await refreshCache();
+
     const epochSec = Date.now() / 1000;
     if ((cache.lockInState.unlockTimeEpoch - epochSec) <= 0) {
         cache.lockInState.lockedIn = false;
@@ -111,6 +128,8 @@ export const getServerLockInState = async () => {
 
 // returns array of filter object
 export const getFilterList = async () => {
+    await refreshCache();
+
     // Return deep copy
     return JSON.parse(JSON.stringify(cache.filterList));
 }
